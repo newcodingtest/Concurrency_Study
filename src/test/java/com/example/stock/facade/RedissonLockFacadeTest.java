962d0731d4c5287bc96738212ac6d6eabb2c3fcf
pulpul8282@naver.com <pulpul8282@naver.com>
2023-06-30 15:24:32 +0900

    redisson을 사용한 동시성 제어

---
+++ src/test/java/com/example/stock/facade/RedissonLockFacadeTest.java
@@ -0,0 +1,60 @@
+package com.example.stock.facade;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class RedissonLockFacadeTest {
+    @Autowired
+    private RedissonLockFacade redissonLockFacade;
+    @Autowired
+    private StockRepository stockRepository;
+    @BeforeEach
+    public void before(){
+        Stock stock = new Stock(1L, 100L);
+
+        stockRepository.saveAndFlush(stock);
+    }
+
+    @AfterEach
+    public void after(){
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    public void redis_setnx_이용한_동시에_100개의_요청() throws InterruptedException {
+        int threadCnt = 100;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(8);
+        CountDownLatch latch = new CountDownLatch(threadCnt);
+
+
+        for (int i=0; i< threadCnt; i++){
+            executorService.submit(() -> {
+                try{
+                    redissonLockFacade.decrease(1l,1l);
+                } finally {
+                    latch.countDown();
+                }
+
+            });
+        }
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        assertEquals(0L,stock.getQuantity());
+
+    }
+}
